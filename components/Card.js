080a1f89e2d0223d23a94a22e98b1803657dac36
Subham Haldar <subham.haldar@appycodes.com>
2022-11-12 12:24:35 +0530

    toastify added

--- components/Card.js
+++ components/Card.js
@@ -4,11 +4,14 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { useAuth } from "../context/AuthContext";
 import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "../config/firebase";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Card = ({ title, desc, authorName, day, month, year, id }) => {
   const { user, data } = useAuth();
   const handleDelete = async () => {
     try {
-      await deleteDoc(doc(db, "blogs", id));
+      await deleteDoc(doc(db, "blogs", id)); toast("Delete Succesfull")
     } catch (error) {
       console.log(error);
     }
@@ -17,37 +20,41 @@ const Card = ({ title, desc, authorName, day, month, year, id }) => {
     console.log(user.uid, data[0].authorId, data);
   };
   return (
-    <div className=" border-2 border-sky-500 p-4 w-1/4 flex flex-col gap-2 rounded-lg">
-      <h1 className="font-mono font-bold">{title}</h1>
-      <section className="flex-1">
-        <p>{desc}</p>
-      </section>
-      <footer className="flex justify-between items-center">
-        <div className="font-mono font-bold">
-          <p>Author : {authorName}</p>
-          <p>
-            {day} {month},{year}
-          </p>
-        </div>
-
-        {user.email === authorName ? (
-          <div className="flex gap-2">
-            <CreateIcon
-              sx={{ width: "20px", height: "20px" }}
-              className=" cursor-pointer"
-              onClick={handleEdit}
-            />
-            <DeleteIcon
-              sx={{ width: "20px", height: "20px" }}
-              className=" cursor-pointer"
-              onClick={handleDelete}
-            />
+    <>
+      <div className=" border-2 border-sky-500 p-4 w-1/4 flex flex-col gap-2 rounded-lg">
+        <h1 className="font-mono font-bold">{title}</h1>
+        <section className="flex-1">
+          <p>{desc}</p>
+        </section>
+        <footer className="flex justify-between items-center">
+          <div className="font-mono font-bold">
+            <p>Author : {authorName}</p>
+            <p>
+              {day} {month},{year}
+            </p>
           </div>
-        ) : (
-          ""
-        )}
-      </footer>
-    </div>
+
+          {user.email === authorName ? (
+            <div className="flex gap-2">
+              <CreateIcon
+                sx={{ width: "20px", height: "20px" }}
+                className=" cursor-pointer"
+                onClick={handleEdit}
+              />
+              <DeleteIcon
+                sx={{ width: "20px", height: "20px" }}
+                className=" cursor-pointer"
+                onClick={handleDelete}
+              />
+            </div>
+          ) : (
+            ""
+          )}
+        </footer>
+      </div>
+      <ToastContainer />
+    </>
+
   );
 };
 
